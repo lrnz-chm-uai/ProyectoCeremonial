bca87d4e8c59692fb9c6619ce0ee4be61bffe651
lrnz-chm-uai <lorenzo.christianimansilla@alumnos.uai.edu.ar>
2026-09-05 10:20:09 -0300

    Primer objeto 3D -plano- y script asociado

---
+++ Assets/Scripts/ScriptDePrueba.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDePrueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("2D or 3D?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
